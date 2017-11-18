c831d83336d649664ce6a72e01afc05ed7845317
Jan Trzcinski <poljant@post.pl>
2017-11-18 23:38:07 +0100

    dodano kilka komentarzy

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 Ocena za pomocą gwiazdek.
 
 autor: Jan Trzciński
-e-mail: poljant.post.pl
+
+e-mail: poljant@post.pl
+
 www: http://poljant.pl
 
 Program na licencji MIT.
@@ -10,6 +12,7 @@ Program na licencji MIT.
 Jak w prosty sposób zrobić na stronie www ocenę za pomocą gwiazdek.
 
 Program korzysta z ikon google "https://fonts.googleapis.com/icon?family=Material+Icons".
+
 Oraz jquery (korzystam z załączonego pliku jquery-3-2-1.min.js).
 
 Strona demo http://ratestars.poljant.pl/index.html.
--- js/stars.js
+++ js/stars.js
@@ -8,31 +8,31 @@ var s5 = document.getElementById('s5');
 
 var starmax = 5;
 var ratingStar = 0;
+// ustalone dostępne kolory 
 var colorGrey = 'grey';
 var colorRed = 'red';
 var colorGreen = 'green';
 var colorYellow = 'yellow';
 var colorBlue = 'blue';
 var colorGold = 'gold';
+//kolor gwiazdek
 var colorStar = colorBlue;
-/*var imgStarEmpty = '&#E83A;'
-var imgStarHover = '$#E838;'
-var imgStarClick = '$#E838;*/
-
+//ustalenie reakcji na klik myszy
 s1.addEventListener("click", function () {starClick(1);});
 s2.addEventListener("click", function () {starClick(2);});
 s3.addEventListener("click", function () {starClick(3);});
 s4.addEventListener("click", function () {starClick(4);});
 s5.addEventListener("click", function () {starClick(5);});
-
+//ustalenie reakcji na opuszczenie pola starx
 s0.addEventListener("mouseout", function () {if (ratingStar === 0) starReset(5);}); 
-
+//ustalenie reakcji na hover myszy (kursor nad elementem)
 s1.addEventListener("mouseover", function () {starMouseover(1);});
 s2.addEventListener("mouseover", function () {starMouseover(2);});
 s3.addEventListener("mouseover", function () {starMouseover(3);});
 s4.addEventListener("mouseover", function () {starMouseover(4);});
 s5.addEventListener("mouseover", function () {starMouseover(5);});
 
+//reset kształtu i koloru gwiazdek
 function starReset(nr) {
 	'use strict';
 	ratingStar = 0;
@@ -44,13 +44,15 @@ function starReset(nr) {
 						 'transition' : 'all .1s ease-in-out'});
 	}
 }
-
+//reakcja na hover (mysza nad elementem)
 function starMouseover(nr) {
 	'use strict';
 	if (ratingStar === 0) {
+		//ustal kolor gwiazdek zaznaczonych
 		for (var j = 1; j <= nr; j++) {
 			$('#s' + j).css('color',colorStar);
 			}
+		//resetuj kolor gwiadek pozostałych
 		for (var i = nr + 1; i <= starmax; i++) {
 			$('#s' + i).css({ 'color' : colorGrey,
 							  'filter' : 'brightness(1)',
@@ -58,17 +60,19 @@ function starMouseover(nr) {
 			}
 	}
 }
-
+//reakcja na przycisk myszy
 function starClick(nr) {
 	'use strict';
 	starReset(starmax);
 	
 	for (var i = 1; i <= nr; i++) {
+		//zmień gwiazdki wybrane
 		$('#s' + i).html('star');
+		//zmień kolor gwiazdek wybranych
 		$('#s' + i).css({'filter':'brightness(1)',
 						 'color' : colorStar,
 						 'transition': 'all .3s ease-in-out'});
 	}
+	//zapamiętaj gwiazdkę wybraną
 	ratingStar = nr;
-	//console.log(ratingStar);
 }
